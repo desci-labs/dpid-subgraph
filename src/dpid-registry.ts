937ce8b337ed28d9a332bead80807a3337a30d1c
Sina Iman <sina.iman@gmail.com>
2023-05-27 12:52:24 +0200

    fix graph config

--- src/dpid-registry.ts
+++ src/dpid-registry.ts
@@ -1,29 +1,30 @@
+import { Value } from "@graphprotocol/graph-ts";
 import {
   Initialized as InitializedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   Register as RegisterEvent,
   RegisterOrganization as RegisterOrganizationEvent,
-  UpdateOrganization as UpdateOrganizationEvent
-} from "../generated/DpidRegistry/DpidRegistry"
+  UpdateOrganization as UpdateOrganizationEvent,
+} from "../generated/DpidRegistry/DpidRegistry";
 import {
   Initialized,
   OwnershipTransferred,
   Register,
   RegisterOrganization,
-  UpdateOrganization
-} from "../generated/schema"
+  UpdateOrganization,
+} from "../generated/schema";
 
 export function handleInitialized(event: InitializedEvent): void {
   let entity = new Initialized(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
+  );
+  entity.version = event.params.version;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -31,29 +32,29 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRegister(event: RegisterEvent): void {
   let entity = new Register(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.prefix = event.params.prefix
-  entity.entryId = event.params.entryId
+  );
+  entity.prefix = event.params.prefix;
+  entity.entryId = event.params.entryId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRegisterOrganization(
@@ -61,29 +62,33 @@ export function handleRegisterOrganization(
 ): void {
   let entity = new RegisterOrganization(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.prefix = event.params.prefix
-  entity.registrant = event.params.registrant
-  // entity.tokenGate = event.params.tokenGate
+  );
+  entity.prefix = event.params.prefix;
+  entity.registrant = event.params.registrant;
+  entity.tokenGate = Value.fromAddressArray(
+    event.params.tokenGate
+  ).toBytesArray();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleUpdateOrganization(event: UpdateOrganizationEvent): void {
   let entity = new UpdateOrganization(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.prefix = event.params.prefix
-  entity.registrant = event.params.registrant
-  // entity.tokenGate = event.params.tokenGate
+  );
+  entity.prefix = event.params.prefix;
+  entity.registrant = event.params.registrant;
+  entity.tokenGate = Value.fromAddressArray(
+    event.params.tokenGate
+  ).toBytesArray();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
